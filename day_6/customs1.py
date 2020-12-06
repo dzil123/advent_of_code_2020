f166638e9ab81bfb4429bc8bf6f439eb67c61979
dzil123 <5725958+dzil123@users.noreply.github.com>
2020-12-05 21:16:39 -0800

    day 6

---
+++ day_6/customs1.py
@@ -0,0 +1,19 @@
+# FILE = "test.txt"
+FILE = "customs.txt"
+
+groups = []
+
+with open(FILE, "r") as f:
+    group = ""
+    for line in f:
+        line = line.strip()
+        if not line:  # newline, end of group
+            ans = len(set(group))
+            groups.append(ans)
+            group = ""
+        group += line # concat all responses within a group to a single string
+    ans = len(set(group)) # deduplicate the letters and count the length
+    groups.append(ans)
+    group = ""
+
+print(sum(groups))
---
+++ day_6/customs2.py
@@ -0,0 +1,22 @@
+import string
+
+# FILE = "test.txt"
+FILE = "customs.txt"
+
+groups = []
+
+with open(FILE, "r") as f:
+    group = set(string.ascii_lowercase) # set of chars, start with every possible item
+    for line in f:
+        line = line.strip()
+        if not line:  # newline, end of group
+            ans = len(group)
+            groups.append(ans)
+            group = set(string.ascii_lowercase)
+            continue
+        group.intersection_update(line) # remove items in group that arent in line
+    ans = len(group)
+    groups.append(ans)
+    group = set(string.ascii_lowercase)
+
+print(sum(groups))

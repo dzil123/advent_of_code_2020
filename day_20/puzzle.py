d6e472fbbbd63b7a70ff952a2e8407c92ab2c239
dzil123 <5725958+dzil123@users.noreply.github.com>
2020-12-21 03:02:23 -0800

    yay it assembled the image

--- day_20/puzzle.py
+++ day_20/puzzle.py
@@ -2,13 +2,13 @@ import re
 import dataclasses
 from pprint import pprint
 from collections import deque, defaultdict, Counter
+import random
 
 FILE = "test.txt"
 # FILE = "test2.txt"
 # FILE = "puzzle.txt"
 
-# 10x10 image
-
+# only the sides
 @dataclasses.dataclass(frozen=True)
 class Tile:
     north: tuple[str] # left to right
@@ -96,20 +96,23 @@ def read_tile(f):
 
     tile = []
     while (line := f.readline().strip()):
-        row = [char == "#" for char in line if char in ".#"]
+        # row = [char == "#" for char in line if char in ".#"]
+        row = line
         tile.append(row)
     
-    return tile_id, Tile.make(tile)
+    return tile_id, tile, Tile.make(tile)
 
 def read():
     tiles = {}
+    raw_tiles = {}
 
     with open(FILE, "r") as f:
         while (tile := read_tile(f)) is not None:
-            tile_id, tile = tile
+            tile_id, raw_tile, tile = tile
             tiles[tile_id] = tile
+            raw_tiles[tile_id] = raw_tile
     
-    return tiles
+    return raw_tiles, tiles
 
 def generate_orientations(tile, hist=None, seen=None):
     if hist is None:
@@ -137,13 +140,13 @@ def same_tiles(tiles, sides):
     
     return variable
 
-def part1():
-    tiles = read()
+def part1(data):
+    _, tiles = data
 
     n_s = same_tiles(tiles, (0, 1))
     e_w = same_tiles(tiles, (2, 3))
 
-    pprint(n_s)
+    # pprint(n_s)
     # print()
     # pprint(e_w)
 
@@ -173,17 +176,17 @@ def adjacency(n_s, e_w):
             if len(touching) != 2:
                 continue
             touching = list(touching.items())
-            print(touching)
+            # print(touching)
             for this, other in ((touching[0], touching[1]), (touching[1], touching[0])):
                 this_id, this_sides = this
                 other_sides = other[1]
 
-                print(this_id, this_sides, other_sides)
+                # print(this_id, this_sides, other_sides)
 
                 for this_side in this_sides:
                     for other_side in other_sides:
                         if this_side.side != other_side.side:
-                            print(" ", this_id, this_side, other_side)
+                            # print(" ", this_id, this_side, other_side)
                             tiles[this_id][this_side] = other_side
 
     # for key, value in tiles.items():
@@ -191,17 +194,94 @@ def adjacency(n_s, e_w):
 
     return tiles
 
-
-def part2():
-    tiles = read()
-
+def assemble_tiles(tiles):
     n_s = same_tiles(tiles, (0, 1))
     e_w = same_tiles(tiles, (2, 3))
 
     adjacent = adjacency(n_s, e_w)
-    pprint(adjacent)
+
+    random_tile_id = random.choice(list(adjacent.keys())) # pick random tile to be at the center
+    random_tile_orientation = random.choice(list(adjacent[random_tile_id].keys())).tile # and random orientation
+
+    image = {} # 2d array but sparse, dict[position(row(int), column(int)), OrientedTile]
+    current_pos = (0, 0)
+    image[current_pos] = random_tile_orientation
+
+    queue = deque((current_pos,))
+    seen = set()
+    SIDE_DIRECTION = ((-1, 0), (1, 0), (0, 1), (0, -1)) # NSEW, (y, x) (row, column)
+
+    while queue:
+        current_pos = queue.pop()
+        current_tile = image[current_pos]
+        next_tiles = {side.side: touching for side, touching in adjacent[current_tile.id].items() if side.tile == current_tile}
+        print(next_tiles)
+
+        for side, touching_side in next_tiles.items():
+            direction = SIDE_DIRECTION[side]
+            next_pos = (current_pos[0] + direction[0], current_pos[1] + direction[1])
+            touching = touching_side.tile
+
+            assert (expected := image.get(next_pos)) is None or expected == touching
+            
+            image[next_pos] = touching
+            if next_pos not in seen:
+                seen.add(next_pos)
+                queue.append(next_pos)
+    
+    return image
+
+def get_bounds(positions):
+    low = [0, 0]
+    high = [0, 0]
+
+    for x, y in positions:
+        if x < low[0]:
+            low[0] = x
+        if x > high[0]:
+            high[0] = x
+        
+        if y < low[1]:
+            low[1] = y
+        if y > high[1]:
+            high[1] = y
+    
+    extents = [high[0] - low[0] + 1, high[1] - low[1] + 1]
+    print(low, high, extents)
+    return low, high, extents
+
+def trim_tile(tile):
+    return [row[1:-1] for row in tile[1:-1]]
+
+def rotate_full_tile(tile, orientation):
+    for rule in orientation:
+        if rule == "h":
+            tile = [row[::-1] for row in tile]
+        elif rule == "r":
+            tile = list(zip(*reversed(tile)))
+    return tile
+
+def part2(data):
+    raw_tiles, tiles = data
+    size = (8, 8) # 10x10 tiles, trimmed on each edge
+    # pprint(raw_tiles)
+
+    image = assemble_tiles(tiles)
+    low, high, extents = get_bounds(list(image.keys()))
+    # pprint(image)
+    print()
+
+    trimmed_raw_tiles = {id: trim_tile(tile) for id, tile in raw_tiles.items()}
+    filled_image = {pos: rotate_full_tile(trimmed_raw_tiles[tile.id], tile.orientation) for pos, tile in image.items()}
+
+    final_image = [[filled_image[low[0]+row//size[0], low[1]+col//size[1]][row%size[0]][col%size[1]] for col in range(size[1] * extents[1]) ] for row in range(size[0] * extents[0])]
+    final_image = [''.join(row) for row in final_image]
+    print('\n'.join(final_image))
 
 
-# pprint(read().popitem())
-# print(part1())
-print(part2())
+def run():
+    data = read()
+    part1(data)
+    part2(data)
+
+run()
